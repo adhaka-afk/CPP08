cd72d36262bc43588cb3529df28a680c0c8ba39a
Arvind Dhaka <adhaka@3-C-10.42heilbronn.de>
2024-07-05 13:22:04 +0200

    drafted ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 08:45:23 by adhaka            #+#    #+#             */
+/*   Updated: 2024/07/05 02:33:13 by adhaka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
+#include <iterator>
+
+template <typename T>
+class MutantStack : 
+
+#endif
