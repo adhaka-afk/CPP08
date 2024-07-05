0b60b402aa44ae6a7d3bab9d340795a84864ec76
Arvind Dhaka <adhaka@3-C-10.42heilbronn.de>
2024-07-05 11:33:26 +0200

    smol fix

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 08:44:59 by adhaka            #+#    #+#             */
-/*   Updated: 2024/07/05 11:28:38 by adhaka           ###   ########.fr       */
+/*   Updated: 2024/07/05 11:31:04 by adhaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int main(int argc, char **argv)
 	// Testing with a vector
 
 	std::vector<int> vec;
-	for (int i = 0; i < 100; i++)
-		vec.push_back(i);
+	for (int a = 0; a < 100; a++)
+		vec.push_back(a);
 
 	try
 	{
@@ -47,25 +47,25 @@ int main(int argc, char **argv)
 	// Testing with a list
 
 	std::list<int> lst;
-	for (int i = 0; i < 10; i++)
-		lst.push_back(i);
+	for (int b = 0; b < 10; b++)
+		lst.push_back(b);
 
 	try
 	{
 		std::istringstream iss(argv[1]);
-		int val;
-		if (!(iss >> val))
+		int value;
+		if (!(iss >> value))
 		{
 			std::cerr << "Invalid input: Not an integer" << std::endl;
 			return 1;
 		}
-		std::list<int>::iterator it = easyfind(lst, val);
+		std::list<int>::iterator it = easyfind(lst, value);
 		std::cout << "Element found in list: " << *it << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	
+
 	return 0;
 }
