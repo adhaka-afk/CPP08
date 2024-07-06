05240cbe38c682ed75f0fecb12a61e7bb38e2a05
Arvind Dhaka <adhaka@3-C-10.42heilbronn.de>
2024-07-06 20:28:16 +0200

    did extensive testing on ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 08:45:23 by adhaka            #+#    #+#             */
-/*   Updated: 2024/07/05 02:33:13 by adhaka           ###   ########.fr       */
+/*   Updated: 2024/07/06 20:26:16 by adhaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,69 @@
 #include <iterator>
 
 template <typename T>
-class MutantStack : 
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::deque<T>::iterator iterator;
+		typedef typename std::deque<T>::const_iterator const_iterator;
+
+		MutantStack();
+		MutantStack(const MutantStack<T>& other);
+		MutantStack<T>& operator=(const MutantStack<T>& other);
+		virtual	~MutantStack();
+
+		iterator begin();
+		iterator end();
+		const_iterator begin() const;
+		const_iterator end() const;
+};
+
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T>()
+{
+}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &other) : std::stack<T>(other)
+{
+}
+
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T> &other)
+{
+	if (this == &other)
+		return (*this);
+	std::stack<T>::operator=(other);
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{
+	return this->c.end();
+}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 08:45:19 by adhaka            #+#    #+#             */
-/*   Updated: 2024/07/05 02:03:06 by adhaka           ###   ########.fr       */
+/*   Updated: 2024/07/06 20:26:26 by adhaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,41 @@
 
 int main()
 {
+	try
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		mstack.push(3);
+		std::cout << "Top: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "Size: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(757);
+		mstack.push(12);
+		mstack.push(10);
+		mstack.push(0);
 
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+
+		std::cout << "Iteratorating through the stack: \n";
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+
+		std::stack<int> s(mstack);
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
 }
